2cfd0f751e9afac3b781dceb944cd5e999428630
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-11-09 10:05:11 -0500

    re-adding index

--- hillandgertner/urls.py
+++ hillandgertner/urls.py
@@ -17,9 +17,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import serve
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^front/', include('hillandgertner.pages.urls')),
 ]
 
