9fcbc3ace65305aae9ddc897ad5e585cf75b8bfb
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-11-22 17:51:43 -0500

    adding css fixes

--- hillandgertner/page_globals/models.py
+++ hillandgertner/page_globals/models.py
@@ -10,6 +10,10 @@ from django.conf import settings
 class Globals(models.Model):
     header_height = models.CharField(max_length=500, blank=False, null=True, default="25.8")
     page_background_color = models.CharField(max_length=500, blank=False, null=True, default="rgba(89,89,89,0.1)")
+    heading_text_size = models.IntegerField(blank=False, null=True, default=10)
+    heading_text_color = models.CharField(max_length=500, blank=False, null=True, default="#000")
+    body_text_size = models.IntegerField(blank=False, null=True, default=12)
+    body_text_color = models.CharField(max_length=500, blank=False, null=True, default="#000")
 
 # method for updating
 @receiver(post_save, sender=Globals, dispatch_uid="touch_tmp")
