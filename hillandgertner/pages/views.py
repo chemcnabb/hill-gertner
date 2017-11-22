51a627a22a57eb1681eb4919cc45c6fc11ef7e81
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2017-11-22 18:05:49 -0500

    adding css fixes

--- hillandgertner/pages/views.py
+++ hillandgertner/pages/views.py
@@ -4,19 +4,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from hillandgertner.pages.models import Page
-# from hillandgertner.page_globals.models import Globals
+from hillandgertner.page_globals.models import Globals
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'main.html'
     pages = Page.objects.order_by('-order')
-    # globals = Globals.objects.first()
+    globals = Globals.objects.first()
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['pages'] = self.pages
-        # context['content_margin'] = ((len(self.pages)+1)*float(self.globals.header_height))-(len(self.pages)/2)-1
-        # context['header_height_adjusted'] = float(self.globals.header_height)-1
-        #
-        # context['globals'] = self.globals
+        context['content_margin'] = ((len(self.pages)+1)*float(self.globals.header_height))-(len(self.pages)/2)-1
+        context['header_height_adjusted'] = float(self.globals.header_height)-1
+
+        context['globals'] = self.globals
         return context
