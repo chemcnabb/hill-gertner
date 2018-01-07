d5e4537e7c3d4a4e1bce0c31ca17cf8f3d43cbfc
Che Daniel Mcnabb <che.mcnabb@sgsco.com>
2018-01-07 17:59:33 -0500

    adding microfixes

--- hillandgertner/urls.py
+++ hillandgertner/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import serve
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
+url(r'^login/$', auth_views.login, name='login'),
+
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^admin/', admin.site.urls),
+
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^front/', include('hillandgertner.pages.urls')),
 ]
